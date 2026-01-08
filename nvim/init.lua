5d804347c37dfefd67439cbfaf0bcebab01097f7
James Young <jyyoung@jyymail.com>
2026-01-07 23:05:17 -0500

    add neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,27 @@
+-- essentials
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.undofile = true
+
+-- indentation
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+
+-- search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+
+-- appearance
+vim.opt.termguicolors = true
+vim.opt.signcolumn = 'yes'
+vim.opt.scrolloff = 8
+vim.opt.wrap = false
+
+-- faster updates
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
